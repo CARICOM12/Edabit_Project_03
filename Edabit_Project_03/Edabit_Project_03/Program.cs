21fd7c24292bcb0f7442f342318802687d382ba6
CARICOM12 <0190a@ukr.net>
2020-05-03 15:09:02 +0300

    Add files via upload

---
+++ Edabit_Project_03/Edabit_Project_03/Program.cs
@@ -0,0 +1,10 @@
+﻿public class Program
+{
+    public static int Sum(int a, int b)
+    {
+        int sum = a + b;
+        return sum;
+    }
+}
+
+//Create a function that takes two numbers as arguments and return their sum.
